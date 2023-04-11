4ed81c29d4831a33a1753ecb693ff221384e5875
Alessio Libardi <alessio.libardi@zupit.it>
2023-04-11 08:01:53 +0200

    chore: remove test libraries

--- packages/ng-anotest/src/lib/ng-anotest.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
-
-@NgModule({
-  imports: [CommonModule, FormsModule]
-})
-export class NgAnotestModule {}
--- packages/ng-test/README.md
+++
@@ -1,7 +0,0 @@
-# ng-test
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test ng-test` to execute the unit tests.

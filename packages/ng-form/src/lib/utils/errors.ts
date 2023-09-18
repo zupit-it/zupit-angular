0ada9ec13bbffc0396b34544525c46d2a9dd0adf
Ivan Morandi <ivan.morandi@zupit.it>
2023-09-18 08:37:44 +0200

    chore(ng-form): fix tests

--- packages/ng-form/src/lib/utils/errors.ts
+++ packages/ng-form/src/lib/utils/errors.ts
@@ -1,7 +1,7 @@
 import { ERROR_EXPECTED_MISSING_VALUE } from '../config'
 import { inject } from '@angular/core'
 
-export class AppException extends Error {
+export class FormException extends Error {
   constructor(public errorCode: string) {
     super(errorCode)
   }
@@ -9,7 +9,7 @@ export class AppException extends Error {
 
 export function getValueOrError<T>(value: T | null | undefined): T {
   if (value == null) {
-    throw new AppException(inject(ERROR_EXPECTED_MISSING_VALUE))
+    throw new FormException(inject(ERROR_EXPECTED_MISSING_VALUE))
   }
   return value
 }

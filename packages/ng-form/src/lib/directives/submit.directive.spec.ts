855f06d0724be3532c7f01d4c975da585ed654a2
Ivan Morandi <ivan.morandi@zupit.it>
2023-09-18 08:27:20 +0200

    chore(ng-form): fix tests

--- packages/ng-form/src/lib/directives/submit.directive.spec.ts
+++ packages/ng-form/src/lib/directives/submit.directive.spec.ts
@@ -1,19 +1,18 @@
-import {SubmitDirective} from './submit.directive'
-import {Component} from "@angular/core";
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
-import {ComponentFixture, TestBed} from "@angular/core/testing";
+import { SubmitDirective } from './submit.directive'
+import { Component } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 
 @Component({
   template: `
-    <form id="form" [form]="form" (ngFormSubmit)="onSubmit($event)">
-      <button id="submit" type="submit"></button>
-    </form>
+    <form id="form" [form]="form" (ngFormSubmit)="onSubmit($event)"></form>
   `
 })
 export class TestComponent {
   readonly form = this.fb.group({
-    id: this.fb.control(undefined, [Validators.required]),
-  });
+    id: this.fb.control(undefined, [Validators.required])
+  })
   constructor(private fb: FormBuilder) {}
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/no-unused-vars
@@ -26,30 +25,37 @@ describe('SubmitDirective', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule],
-      declarations: [TestComponent, SubmitDirective],
-    }).compileComponents();
-    fixture = TestBed.createComponent(TestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      declarations: [TestComponent, SubmitDirective]
+    }).compileComponents()
+    fixture = TestBed.createComponent(TestComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
   })
 
   it('ngFormSubmit should emit a submit event when form is valid', () => {
-    component.form.setValue({id: '123'});
-    expect(component.form.valid).toBeTruthy();
+    component.form.setValue({ id: '123' })
+    expect(component.form.valid).toBeTruthy()
 
-    jest.spyOn(component, 'onSubmit');
-    const submit: HTMLElement = fixture.nativeElement.querySelector('#submit');
-    submit.click();
-    expect(component.onSubmit).toHaveBeenCalled();
+    const directive = fixture.debugElement
+      .query(By.directive(SubmitDirective))
+      .injector.get(SubmitDirective)
+
+    jest.spyOn(directive.ngFormSubmit, 'emit')
+
+    directive.onSubmit(new Event('submit'))
+    expect(directive.ngFormSubmit.emit).toHaveBeenCalled()
   })
 
   it('ngFormSubmit should raise an error on submit if form is invalid', () => {
-    expect(component.form.valid).toBeFalsy();
+    expect(component.form.valid).toBeFalsy()
 
-    jest.spyOn(component, 'onSubmit');
-    const submit: HTMLElement = fixture.nativeElement.querySelector('#submit');
-    submit.click()
-    //TODO: expect(component.onSubmit).toThrowError();
-    expect(component.onSubmit).not.toHaveBeenCalled();
+    const directive = fixture.debugElement
+      .query(By.directive(SubmitDirective))
+      .injector.get(SubmitDirective)
+
+    jest.spyOn(directive.ngFormSubmit, 'emit')
+
+    expect(() => directive.onSubmit(new Event('submit'))).toThrowError()
+    expect(directive.ngFormSubmit.emit).not.toHaveBeenCalled()
   })
 })

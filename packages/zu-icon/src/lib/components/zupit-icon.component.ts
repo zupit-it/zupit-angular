e668edf760da538d842fa16ebec1fe1c2f66b48c
deyan <deyan.koba@zupit.it>
2024-05-08 11:11:26 +0200

    feat(ZCH-59): pr review

--- packages/zu-icon/src/lib/components/zupit-icon.component.ts
+++ packages/zu-icon/src/lib/components/zupit-icon.component.ts
@@ -1,46 +1,50 @@
-import {Component, Inject, InjectionToken, Input} from '@angular/core';
+import { Component, Inject, InjectionToken, Input } from '@angular/core'
 
 export interface IconsEnum {
-    [key: string]: string;
+  [key: string]: string
 }
 
-export const CUSTOM_ICONS_ENUM = new InjectionToken<IconsEnum>('CUSTOM_ICONS_ENUM');
+export const CUSTOM_ICONS_ENUM = new InjectionToken<IconsEnum>(
+  'CUSTOM_ICONS_ENUM'
+)
 
 @Component({
-    selector: 'zu-icon',
-    templateUrl: './zupit-icon.component.html',
-    styleUrls: ['./zupit-icon.component.scss'],
+  selector: 'zu-icon',
+  templateUrl: './zupit-icon.component.html',
+  styleUrls: ['./zupit-icon.component.scss']
 })
 export class ZupitIconComponent {
-    private _icon: string;
-    private _iconsNames: string[] = [];
+  private _icon: string
+  private _iconsNames: string[] = []
 
-    @Input() set icon(value: string) {
-        if (!this.validateIcon(value)) {
-            throw new Error(`Invalid icon '${value}' provided.`);
-        }
-        this._icon = value;
+  @Input() set icon(value: string) {
+    if (!this.validateIcon(value)) {
+      throw new Error(`Invalid icon '${value}' provided.`)
+    }
+    this._icon = value
+  }
+
+  get icon(): string {
+    return this._icon
+  }
+
+  private validateIcon(icon: string): boolean {
+    return this._iconsNames.includes(icon)
+  }
+
+  constructor(@Inject(CUSTOM_ICONS_ENUM) public icons: IconsEnum) {
+    if (!icons) {
+      throw new Error(
+        'CUSTOM_ICONS_ENUM not provided. Please provide the Icons enum.'
+      )
     }
 
-    get icon(): string {
-        return this._icon;
+    if (typeof icons !== 'object') {
+      throw new Error(
+        'CUSTOM_ICONS_ENUM not valid. Please provide a valid Icons enum.'
+      )
     }
 
-    private validateIcon(icon: string): boolean {
-        return this._iconsNames.includes(icon);
-    }
-
-    constructor(@Inject(CUSTOM_ICONS_ENUM) public icons: IconsEnum) {
-        //todo save object values in a var
-
-        if (!icons) {
-            throw new Error('CUSTOM_ICONS_ENUM not provided. Please provide the Icons enum.');
-        }
-
-        if(typeof icons !== "object") {
-            throw new Error('CUSTOM_ICONS_ENUM not valid. Please provide a valid Icons enum.');
-        }
-
-        this._iconsNames = Object.values(icons);
-    }
+    this._iconsNames = Object.values(icons)
+  }
 }

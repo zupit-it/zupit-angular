c392f112fe8dc7847e3254f442dc715178996739
deyan <deyan.koba@zupit.it>
2024-05-09 10:03:51 +0200

    feat(ZCH-59): move constructor checks to forRoot function

--- packages/ng-fantasticon/src/lib/ng-fantasticon.module.ts
+++ packages/ng-fantasticon/src/lib/ng-fantasticon.module.ts
@@ -1,10 +1,40 @@
-import { NgModule } from '@angular/core'
+import { ModuleWithProviders, NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { NgFantasticonComponent } from './components/ng-fantasticon.component'
+import {
+  CUSTOM_ICONS_ENUM,
+  ICONS_NAMES,
+  IconsEnum,
+  NgFantasticonComponent
+} from './components/ng-fantasticon.component'
 
 @NgModule({
   imports: [CommonModule],
   declarations: [NgFantasticonComponent],
   exports: [NgFantasticonComponent]
 })
-export class NgFantasticonModule {}
+export class NgFantasticonModule {
+  static forRoot(icons: IconsEnum): ModuleWithProviders<NgFantasticonModule> {
+    if (!icons) {
+      throw new Error(
+        'CUSTOM_ICONS_ENUM not provided. Please provide the Icons enum.'
+      )
+    }
+
+    if (typeof icons !== 'object') {
+      throw new Error(
+        'CUSTOM_ICONS_ENUM not valid. Please provide a valid Icons enum.'
+      )
+    }
+
+    const iconsArray = Object.values(icons)
+    const iconsNames = new Set<string>(iconsArray)
+
+    return {
+      ngModule: NgFantasticonModule,
+      providers: [
+        { provide: CUSTOM_ICONS_ENUM, useValue: icons },
+        { provide: ICONS_NAMES, useValue: iconsNames }
+      ]
+    }
+  }
+}
